f06faf1191924adfba3706c5322dd4ecde311fca
Manuel Lara <manuellaraa@gmail.com>
2022-08-25 14:41:50 -0700

    added authProvider

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,31 +1,43 @@
-import React from "react"
-import { fetchUtils, Admin, Resource } from "react-admin"
-import postgrestRestProvider from "@promitheus/ra-data-postgrest"
-import PostList from "../components/PostList"
-import CommentList from "../components/CommentList"
-import CommentEdit from "../components/CommentEdit"
-import CommentCreate from "../components/CommentCreate"
+import React from "react";
+import { fetchUtils, Admin, Resource } from "react-admin";
+import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+import PostList from "../components/PostList";
+import CommentList from "../components/CommentList";
+import CommentEdit from "../components/CommentEdit";
+import CommentCreate from "../components/CommentCreate";
+import authProvider from "../auth/authProvider";
 
 const httpClient = (url, options = {}) => {
   if (!options.headers) {
-    options.headers = new Headers({ Accept: "application/json" })
+    options.headers = new Headers({ Accept: "application/json" });
   }
 
   // set the anon key header
-  options.headers.set("apikey", `${process.env.REACT_APP_ANON_KEY}`)
+  options.headers.set("apikey", `${process.env.REACT_APP_ANON_KEY}`);
 
-  return fetchUtils.fetchJson(url, options)
-}
+  // set the jwt authorization header
+  if (JSON.parse(sessionStorage.getItem("supabaseSession"))) options.headers.set("Authorization", `Bearer ${JSON.parse(sessionStorage.getItem("supabaseSession")).access_token}`)
 
-const dataProvider = postgrestRestProvider(`${process.env.REACT_APP_SUPABASE_URL}`, httpClient)
+  return fetchUtils.fetchJson(url, options);
+};
+
+const dataProvider = postgrestRestProvider(
+  `${process.env.REACT_APP_SUPABASE_URL}/rest/v1/`,
+  httpClient
+);
 
 const App = () => {
   return (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
       <Resource name="posts" list={PostList} />
-      <Resource name="comments" list={CommentList} edit={CommentEdit} create={CommentCreate} />
+      <Resource
+        name="comments"
+        list={CommentList}
+        edit={CommentEdit}
+        create={CommentCreate}
+      />
     </Admin>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/auth/authProvider.js
@@ -0,0 +1,54 @@
+const authProvider = {
+  // send username and password to the auth server and get back credentials
+  login: async ({ username, password }) => {
+    await fetch(
+      `${process.env.REACT_APP_SUPABASE_URL}/auth/v1/token?grant_type=password`,
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          apikey: process.env.REACT_APP_ANON_KEY,
+        },
+        body: JSON.stringify({
+          email: username,
+          password: password,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        sessionStorage.setItem("supabaseSession", JSON.stringify(result))
+      );
+
+    if (!sessionStorage.getItem("supabaseSession"))
+      return Promise.reject({ message: "error" });
+
+    if (sessionStorage.getItem("supabaseSession")) return Promise.resolve();
+  },
+
+  // when the dataProvider returns an error, check if this is an authentication error
+  checkError: (error) => Promise.resolve(error),
+
+  // when the user navigates, make sure that their credentials are still valid
+  checkAuth: () => {
+    if (sessionStorage.getItem("supabaseSession")) return Promise.resolve();
+
+    return Promise.reject({ message: "no user" });
+  },
+
+  // remove local credentials and notify the auth server that the user logged out
+  logout: async () => {
+    sessionStorage.removeItem("supabaseSession");
+
+    return Promise.resolve();
+  },
+
+  // get the user's profile
+  getIdentity: () =>
+    Promise.resolve(JSON.parse(sessionStorage.getItem("supabaseSession")).user),
+
+  // get the user permissions (optional)
+  getPermissions: () => Promise.resolve(""),
+};
+
+export default authProvider;
